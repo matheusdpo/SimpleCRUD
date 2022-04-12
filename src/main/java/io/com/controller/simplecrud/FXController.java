2673a4212988d791f96ba6680387a33943893a16
mathe <mathe@WinXNote>
2022-04-12 19:46:13 -0300

    all create variables were added

--- src/main/java/io/com/controller/simplecrud/FXController.java
+++ src/main/java/io/com/controller/simplecrud/FXController.java
@@ -6,6 +6,9 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class FXController implements Initializable {
@@ -52,6 +55,60 @@ public class FXController implements Initializable {
 
 	@FXML
 	private TextField txtFieldCreateID;
+
+	@FXML
+	private TextField txtFieldCreateName;
+	
+	@FXML
+	private TextField txtFieldCreateSurname;
+	
+	@FXML
+	private TextField txtFieldCreateUsername;
+	
+	@FXML
+	private PasswordField pwFieldCreatePassword;
+	
+	@FXML
+	private ChoiceBox<String> chcCreateMartialStatus;
+	
+	@FXML
+	private TextField txtFieldCreateRG;
+	
+	@FXML
+	private TextField txtFieldCreateCPF;
+	
+	@FXML
+	private DatePicker datePickerCreateDateofBirth;
+	
+	@FXML
+	private TextField txtFieldCreateAge;
+	
+	@FXML
+	private ChoiceBox<String> chcCreateUF;
+	
+	@FXML
+	private TextField txtFieldCreateZIPCode;
+	
+	@FXML
+	private TextField txtFieldCreateAdress;
+	
+	@FXML
+	private TextField txtFieldCreateDisrtict;
+	
+	@FXML
+	private TextField txtFieldCreateHouseNumber;
+	
+	@FXML
+	private TextField txtFieldCreateCity;
+	
+	@FXML
+	private TextField txtFieldCreateEMail;
+
+	@FXML
+	private TextField txtFieldCreateDDD;
+	
+	@FXML
+	private TextField txtFieldCreatePhone;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
