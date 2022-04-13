03f4668e1c0b03340116fc52327e63d44b3b0674
matheusdpo <matheusoliveira1991@hotmail.com>
2022-04-13 12:39:08 -0300

    normal

--- src/main/java/io/com/crud/simplecrud/CRUD.java
+++ src/main/java/io/com/crud/simplecrud/CRUD.java
@@ -5,6 +5,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import io.com.connection.simplecrud.ConnectionSQL;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class CRUD {
 
