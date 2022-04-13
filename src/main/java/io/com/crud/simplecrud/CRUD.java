bee77a4af50590b9fefb749c466518474ff9fcb4
matheusdpo <matheusoliveira1991@hotmail.com>
2022-04-13 13:50:29 -0300

    delete all not used imports

--- src/main/java/io/com/crud/simplecrud/CRUD.java
+++ src/main/java/io/com/crud/simplecrud/CRUD.java
@@ -5,12 +5,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import io.com.connection.simplecrud.ConnectionSQL;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 
 public class CRUD {
 
@@ -38,19 +32,24 @@ public class CRUD {
 		}
 	}
 
-	/*public void rename(String first_name, String surname, String username, String user_password, String martial_status,
-			String rg, String cpf, String date_of_birthh, String age, String state, String zipcode, String address,
-			String district, String house_number, String city, String email, String ddd, String phone_number) {
-	}
-
-	public void update(String first_name, String surname, String username, String user_password, String martial_status,
-			String rg, String cpf, String date_of_birthh, String age, String state, String zipcode, String address,
-			String district, String house_number, String city, String email, String ddd, String phone_number) {
-	}
-
-	public void delete(String first_name, String surname, String username, String user_password, String martial_status,
-			String rg, String cpf, String date_of_birthh, String age, String state, String zipcode, String address,
-			String district, String house_number, String city, String email, String ddd, String phone_number) {
-	}*/
+	/*
+	 * public void rename(String first_name, String surname, String username, String
+	 * user_password, String martial_status, String rg, String cpf, String
+	 * date_of_birthh, String age, String state, String zipcode, String address,
+	 * String district, String house_number, String city, String email, String ddd,
+	 * String phone_number) { }
+	 * 
+	 * public void update(String first_name, String surname, String username, String
+	 * user_password, String martial_status, String rg, String cpf, String
+	 * date_of_birthh, String age, String state, String zipcode, String address,
+	 * String district, String house_number, String city, String email, String ddd,
+	 * String phone_number) { }
+	 * 
+	 * public void delete(String first_name, String surname, String username, String
+	 * user_password, String martial_status, String rg, String cpf, String
+	 * date_of_birthh, String age, String state, String zipcode, String address,
+	 * String district, String house_number, String city, String email, String ddd,
+	 * String phone_number) { }
+	 */
 
 }
